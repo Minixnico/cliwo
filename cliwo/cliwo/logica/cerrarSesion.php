5e339780cb46a99ad48fd676411a709e38bdaf51
Minixnico <104688710+Minixnico@users.noreply.github.com>
2024-03-14 16:58:18 +0100

    Delete cliwo directory

--- cliwo/cliwo/logica/cerrarSesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start(); // Inicia una nueva sesión o reanuda la existente. Es necesario para poder acceder a las variables de sesión y luego poder limpiarlas.
-
-session_unset(); // Limpia todas las variables de sesión. Esto elimina todos los datos registrados en las variables de sesión, pero la sesión en sí sigue activa.
-
-session_destroy(); // Destruye toda la información asociada con la sesión actual. Esto termina efectivamente la sesión, eliminando la sesión del servidor.
-
-header('Location: ../login.php'); // Redirecciona al usuario a la página de inicio de sesión. Esto se hace después de cerrar la sesión para
